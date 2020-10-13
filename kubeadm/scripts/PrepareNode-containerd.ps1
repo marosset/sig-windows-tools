3184817659a47c9b75b8d806e37e88d613cb8e4c
marosset <marosset@microsoft.com>
2020-10-13 14:38:42 -0700

    Updates to flannel-overlay-containerd.yml and preparenode-containerd.ps1

--- kubeadm/scripts/PrepareNode-containerd.ps1
+++ kubeadm/scripts/PrepareNode-containerd.ps1
@@ -36,11 +36,6 @@ if (!$KubernetesVersion.StartsWith("v")) {
     $KubernetesVersion = "v" + $KubernetesVersion
 }
 
-<#
-if (!$ContainerDVersion.StartsWith("v")) {
-    $ContainerDVersion = "v" + $ContainerDVersion
-}
-
 Write-Host "Using Kubernetes version: $KubernetesVersion"
 $global:Powershell = (Get-Command powershell).Source
 $global:PowershellArgs = "-ExecutionPolicy Bypass -NoProfile"
@@ -57,93 +52,19 @@ DownloadFile $kubeletBinPath https://dl.k8s.io/$KubernetesVersion/bin/windows/am
 DownloadFile "$global:KubernetesPath\kubeadm.exe" https://dl.k8s.io/$KubernetesVersion/bin/windows/amd64/kubeadm.exe
 DownloadFile "$global:KubernetesPath\wins.exe" https://github.com/rancher/wins/releases/download/v0.0.4/wins.exe
 
-$global:ConainterDPath = "$env:ProgramFiles\containerd"
-mkdir -force $global:ConainterDPath
-DownloadFile "$global:ConainterDPath\containerd.tar.gz" https://github.com/containerd/containerd/releases/download/v1.4.1/containerd-1.4.1-windows-amd64.tar.gz
-tar.exe -xvf "$global:ConainterDPath\containerd.tar.gz" --strip=1 -C $global:ConainterDPath
-$env:Path += ";$global:ConainterDPath"
-[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-containerd.exe config default | Out-File "$global:ConainterDPath\config.toml" -Encoding ascii
-#config file fixups
-$config = Get-Content "$global:ConainterDPath\config.toml"
-$config = $config -replace "bin_dir = (.)*$", "bin_dir = `"c:/opt/cni/bin`""
-$config = $config -replace "conf_dir = (.)*$", "conf_dir = `"c:/etc/cni/net.d`""
-$config | Set-Content "$global:ConainterDPath\config.toml" -Force 
-#containerd.exe --register-service
-#Get-Service -Name "containerd" | Start-Service
 
-#>
-
-<#
-mkdir -force c:\opt\cni\bin
-mkdir -force c:\etc\cni\net.d
-DownloadFile c:\opt\cni\cni-plugins.tgz https://github.com/containernetworking/plugins/releases/download/v0.8.7/cni-plugins-windows-amd64-v0.8.7.tgz
-tar -xvf c:\opt\cni\cni-plugins.tgz -C c:\opt\cni\bin
-
-@"
-{
-    "name":  "flannel.4096",
-    "cniVersion":  "0.3.0",
-    "type":  "flannel",
-    "capabilities":  {
-                         "dns":  true
-                     },
-    "delegate":  {
-                     "type":  "win-overlay",
-                     "policies":  [
-                                      {
-                                          "Name":  "EndpointPolicy",
-                                          "Value":  {
-                                                        "Type":  "OutBoundNAT",
-                                                        "ExceptionList":  [
-                                                                              "10.96.0.0/12",
-                                                                              "10.244.0.0/16"
-                                                                          ]
-                                                    }
-                                      },
-                                      {
-                                          "Name":  "EndpointPolicy",
-                                          "Value":  {
-                                                        "Type":  "ROUTE",
-                                                        "DestinationPrefix":  "10.96.0.0/12",
-                                                        "NeedEncap":  true
-                                                    }
-                                      }
-                                  ]
-                 }
-}
-"@ | Set-Content c:\etc\cni\net.d\net.json -Force
-
-DownloadFile $global:KubernetesPath\hns.psm1 https://github.com/Microsoft/SDN/raw/master/Kubernetes/windows/hns.psm1
-Import-Module $global:KubernetesPath\hns.psm1
-Get-HnsNetwork | Remove-HnsNetwork
+# DownloadFile $global:KubernetesPath\hns.psm1 https://github.com/Microsoft/SDN/raw/master/Kubernetes/windows/hns.psm1
+# Import-Module $global:KubernetesPath\hns.psm1
+#Get-HnsNetwork | Remove-HnsNetwork
 #New-HnsNetowrk -Type NAT -Name host
-
-New-HNSNetwork -Type Overlay -AddressPrefix "192.168.255.0/30" -Gateway "192.168.255.1" -Name "External" -AdapterName "$AdapterName" -SubnetPolicies @(@{Type = "VSID"; VSID = 9999; })
-
-# set up flannel
-DownloadFile c:\k\flanneld.exe https://github.com/coreos/flannel/releases/download/v0.12.0/flanneld.exe
-New-Item C:\etc\kube-flannel\ -Force -ItemType Directory | Out-Null
-@"
-{
-  "Network": "10.244.0.0/16",
-  "Backend": {
-    "Type": "vxlan",
-    "VNI": 4096,
-    "Port": 4789
-  }
-}
-"@ | Set-Content C:\etc\kube-flannel\net-conf.json -Force | Out-Null
-
-#>
 
 ## Create host network to allow kubelet to schedule hostNetwork pods
 #Write-Host "Creating Docker host network"
 #docker network create -d nat host
 
-#Write-Host "Registering wins service"
-#wins.exe srv app run --register
-#start-service rancher-wins
+Write-Host "Registering wins service"
+wins.exe srv app run --register
+start-service rancher-wins
 
 mkdir -force C:\var\log\kubelet
 mkdir -force C:\var\lib\kubelet\etc\kubernetes
