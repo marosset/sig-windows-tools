32d665f93a287394734a4b6488753575c647958f
Mark Rossetti <marosset@microsoft.com>
2026-06-02 12:04:51 -0700

    Fix test-kube-proxy-images.ps1
    Signed-off-by: Mark Rossetti <marosset@microsoft.com>

--- hack/test-kube-proxy-images.ps1
+++ hack/test-kube-proxy-images.ps1
@@ -2,9 +2,24 @@ param (
     [string]$minK8sVersion = "v1.22"
 )
 
+function Get-K8sVersionCore($tag) {
+    $tagMatch = [regex]::Match($tag, "^v?(?<version>\d+\.\d+(?:\.\d+)?(?:\.\d+)?)(?:-.+)?$")
+    if (-not $tagMatch.Success) {
+        return $null
+    }
+
+    return [Version]$tagMatch.Groups["version"].Value
+}
+
 # Get kube-proxy images
 $content = curl.exe -L registry.k8s.io/v2/kube-proxy/tags/list
 $json = ConvertFrom-Json $content
+
+$minVersion = Get-K8sVersionCore $minK8sVersion
+if ($null -eq $minVersion) {
+    Write-Error "Invalid minimum Kubernetes version: $minK8sVersion"
+    exit 1
+}
 
 $missingKubeProxyImages = @()
 
@@ -12,7 +27,12 @@ foreach ($tag in $json.tags) {
     if (-not($tag.StartsWith("v"))) {
         continue
     }
-    if ([Version]($tag.TrimStart('v')) -lt [Version]($minK8sVersion.TrimStart('v'))) {
+
+    $tagVersion = Get-K8sVersionCore $tag
+    if ($null -eq $tagVersion) {
+        continue
+    }
+    if ($tagVersion -lt $minVersion) {
         continue
     }
 
@@ -21,13 +41,13 @@ foreach ($tag in $json.tags) {
         Write-Output "Checking for image $image"
         docker manifest inspect $image | Out-Null
         if ($LastExitCode -ne 0) {
-            $missingKubeProxyimages += $image
+            $missingKubeProxyImages += $image
             Write-Output "  Image $image is missing!"
         }
     }
 }
 
 if ($missingKubeProxyImages.Length -gt 0) {
-    Write-Output "Found ${$missingKubeProxyImages.Length} missing images!"
+    Write-Output "Found $($missingKubeProxyImages.Length) missing images!"
     exit 1
 }
